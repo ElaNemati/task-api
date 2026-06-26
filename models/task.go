697b62c9a8ad7c0d01fcd3f8c5daa264ec05278c
El <elahw.nemati@gmail.com>
2026-06-26 15:58:47 +0330

    setup database connection

---
+++ models/task.go
@@ -0,0 +1,20 @@
+package models
+
+import "time"
+
+type Status string
+
+const (
+	StatusTodo       Status = "todo"
+	StatusInProgress Status = "in progress"
+	StatusDone       Status = "done"
+)
+
+type Task struct {
+	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title       string    `json:"title" gorm:"not null"`
+	Description string    `json:"description"`
+	Status      Status    `json:"status" gorm:"type:varchar(20);default:'todo'"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
