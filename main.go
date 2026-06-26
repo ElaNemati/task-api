19342140a5d5cdd9c12cd09a6956e89d47b593b4
El <elahw.nemati@gmail.com>
2026-06-26 16:02:20 +0330

    add routes and main entry point

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"task-api/database"
+	"task-api/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
 	}
+
+	database.Connect()
+
+	r := gin.Default()
+
+	r.POST("/tasks", handlers.CreateTask)
+	r.GET("/tasks", handlers.GetAllTasks)
+	r.GET("/tasks/:id", handlers.GetTaskByID)
+	r.PUT("/tasks/:id", handlers.UpdateTask)
+	r.DELETE("/tasks/:id", handlers.DeleteTask)
+
+	r.Run(":8080")
 }
