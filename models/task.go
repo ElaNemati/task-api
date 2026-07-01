9a1386d2322ec20663ef76f71cece0b2ed3ead9a
El <elahw.nemati@gmail.com>
2026-07-02 01:41:48 +0330

    add soft delete

--- models/task.go
+++ models/task.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+
+	"time"
+
+)
 
 type Status string
 
@@ -11,10 +16,11 @@ const (
 )
 
 type Task struct {
-	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`
-	Title       string    `json:"title" gorm:"unique;not null"`
-	Description string    `json:"description"`
-	Status      Status    `json:"status" gorm:"type:varchar(20);default:'todo'"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title       string         `json:"title" gorm:"unique;not null"`
+	Description string         `json:"description"`
+	Status      Status         `json:"status" gorm:"type:varchar(20);default:'todo'"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
 }
